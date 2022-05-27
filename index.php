65e6ee1d9a96c1d2a6dc2eb2d67711d907de42bd
gustavoxrib <gus.ribeiro26@gmail.com>
2022-05-27 13:51:36 -0300

    criação do banco de dados php

--- index.php
+++ index.php
@@ -1,3 +1,62 @@
+<?php
+
+$Host = 'localhost'; 
+$Username = 'root';
+$Password = '';
+$Name = 'formulario_dev';
+
+$conexao = mysqli_connect ($Host, $Username, $Password, $Name) or die ("Erro de Conexão");
+
+if(isset($_POST['submit'])) {
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $cidade = $_POST['cidade'];
+    $estado = $_POST['estado'];
+    $aplicacao = $_POST['aplicacao'];
+    $senior = $_POST['nivel'];     
+    $experiencia = $_POST['experiencia'];
+
+    $result = mysqli_query ($conexao, "INSERT INTO devs (nome, email, cidade, estado, aplicacao, senioridade, experiencia, criacao)
+    VALUES ('$nome', '$email', '$cidade', '$estado', '$aplicacao', '$senior', '$experiencia', NOW())");
+
+    if ($result)
+    {
+        echo 'Conexão com o Banco de Dados efetuada com sucesso.';
+    }
+    else
+    {
+        echo 'Não foi possível cadastrar.';
+    }
+
+    if ($conexao->connect_errno)
+    {
+        echo "Houve um erro: ".mysqli_connect_error();
+    }
+    else
+    {
+        echo "Conexão efetuada com sucesso.";
+    }
+    
+    mysqli_close($conexao);
+    
+    //print_r($_POST['nome']);
+    //print_r('<br>');
+    //print_r($_POST['email']);
+    //print_r('<br>');
+    //print_r($_POST['cidade']);
+    //print_r('<br>');
+    //print_r($_POST['estado']);
+    //print_r('<br>');
+    //print_r($_POST['aplicacao']);
+    //print_r('<br>');
+    //print_r($_POST['nivel']);
+    //print_r('<br>');
+    //print_r($_POST['experiencia']);
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -6,12 +65,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="index.css" media="screen">
-    <title>Cadastro Dev's</title>
+    <title>Cadastro de Dev's</title>
 </head>
 
 <body>
     <div class="box">
-        <form action="">
+        <form action="" method="POST">
             <fieldset>
                 <legend><a href="https://github.com/gustavoxrib/formulario.git"><b>Cadastro de Dev's (GitHub)</b></a>
                 </legend>
@@ -33,7 +92,7 @@
                     </div>
                     <div class="inputbox">
                         <input type="text" name="estado" id="estado" class="inputuser" required>
-                        <label for="estado" class="labelinput">Estado</label>
+                        <label for="estado" class="labelinput">UF</label>
                     </div>
                 </div>
                 <br>
@@ -47,7 +106,7 @@
                 <br>
                 <div class="senioridade">
                     <label class="senior"><b>Senioridade:</b></label>
-                    <select>
+                    <select name="nivel">
                         <option>Selecione</option>
                         <option>Junior</option>
                         <option>Pleno</option>
@@ -62,35 +121,35 @@
                 <br>
                 <div class="option">
                     <div class="grade">
-                        <input type="checkbox" id="tecnologia1" name="tecnologia1" value="HTML">
+                        <input type="checkbox" id="tecnologia1" name="tecnologia[]" value="HTML">
                         <label for="tecnologia1">HTML</label>
                     </div>
                     <div class="grade">
-                        <input type="checkbox" id="tecnologia2" name="tecnologia2" value="CSS">
+                        <input type="checkbox" id="tecnologia2" name="tecnologia[]" value="CSS">
                         <label for="tecnologia2">CSS</label>
                     </div>
                     <div class="grade">
-                        <input type="checkbox" id="tecnologia3" name="tecnologia3" value="Javascript">
+                        <input type="checkbox" id="tecnologia3" name="tecnologia[]" value="Javascript">
                         <label for="tecnologia3">JavaScript</label>
                     </div>
                     <div class="grade">
-                        <input type="checkbox" id="tecnologia4" name="tecnologia4" value="PHP">
+                        <input type="checkbox" id="tecnologia4" name="tecnologia[]" value="PHP">
                         <label for="tecnologia4">PHP</label>
                     </div>
                     <div class="grade">
-                        <input type="checkbox" id="tecnologia5" name="tecnologia5" value="C#">
+                        <input type="checkbox" id="tecnologia5" name="tecnologia[]" value="C#">
                         <label for="tecnologia5">C#</label>
                     </div>
                     <div class="grade">
-                        <input type="checkbox" id="tecnologia6" name="tecnologia6" value="Python">
+                        <input type="checkbox" id="tecnologia6" name="tecnologia[]" value="Python">
                         <label for="tecnologia6">Python</label>
                     </div>
                     <div class="grade">
-                        <input type="checkbox" id="tecnologia7" name="tecnologia7" value="Java">
+                        <input type="checkbox" id="tecnologia7" name="tecnologia[]" value="Java">
                         <label for="tecnologia7">Java</label>
                     </div>
                     <div class="grade">
-                        <input type="checkbox" id="tecnologia8" name="tecnologia8" value="Getz">
+                        <input type="checkbox" id="tecnologia8" name="tecnologia[]" value="Getz">
                         <label for="tecnologia8">Getz</label>
                     </div>
                 </div>
@@ -101,9 +160,9 @@
                 <div>
                     <textarea rows="4" id="experiencia" name="experiencia"></textarea>
                     <br>
-                    <div class="botao">
+                    <div class="botao">                        
+                        <button type="submit" name="submit" id="concluido">Concluído</button>
                         <button type="reset" id="limpar">Limpar</button>
-                        <button type="submit" id="concluido">Concluído</button>
                     </div>
             </fieldset>
         </form>
